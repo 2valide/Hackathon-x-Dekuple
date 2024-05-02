e71acca1bdee4f9f32d5339214247dbbd5c30e1a
Hugo <h.pigreepro@gmail.com>
2024-05-02 15:20:21 +0200

    sidebarInit

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,7 +1,8 @@
 const Sidebar =()=>{
     return(
-        <div className="w-[10rem] h-[40rem] bg-[#35264A]">
-            <img src="/logodekuple.svg" alt="" />
+        <div className="w-[10rem] h-[40rem] bg-[#35264A]"> 
+            <img src="@/img/logodekuple.svg" alt="" />
+            <div className=""></div>
         </div>
     )
 }
