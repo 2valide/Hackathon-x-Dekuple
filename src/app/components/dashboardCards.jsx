ddef5693b7e49662fa1ac7f598d44a3ee879fa54
LDNCQ <willylin11@gmail.com>
2024-05-02 16:38:51 +0200

    cards

--- src/app/components/dashboardCards.jsx
+++ src/app/components/dashboardCards.jsx
@@ -12,10 +12,12 @@ import Link from "next/link";
 export default function Cards({company}) {
     return (
     <div>
-        <div className="flex bg-black">
-            <Link href={company.website}><img src={company.logo} alt={company.alt}/></Link>
-            <div className="">
-                <h1 className="text-5xl">{company.name}</h1>
+        <div className="flex w-[1090px] h-[205px] bg-[#009699] rounded-md">
+            <div className="w-[300px] h-[185px] bg-[#230F49] my-2.5 mx-2.5 flex justify-center items-center rounded-md border border-white">
+                <Link href={company.website}><img src={company.logo} alt={company.alt} className="w-[120px] h-[120px]"/></Link>
+            </div>
+            <div className="text-white flex flex-col justify-between my-2.5 font-medium">
+                <h1 className="text-5xl font-bold">{company.name}</h1>
                 <div className="flex justify-between text-base">
                     <p className="text-base">Status : {company.status}</p>
                     <p>Voir plus</p>
