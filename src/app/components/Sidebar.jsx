266163f3f32002994a738051dc402ae4c7494082
Hugo <h.pigreepro@gmail.com>
2024-05-02 15:27:10 +0200

    test

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -2,7 +2,7 @@ const Sidebar =()=>{
     return(
         <div className="w-[10rem] h-[40rem] bg-[#35264A]"> 
             <img src="@/img/logodekuple.svg" alt="" />
-            <div className=""></div>
+            <div className="">dsqds</div>
         </div>
     )
 }
