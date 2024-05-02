c351e89c7b46fda1ef7ae9a2a003781541f3c26f
Hugo <h.pigreepro@gmail.com>
2024-05-02 16:48:03 +0200

    Description des modifications

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -19,15 +19,3 @@ const Sidebar =()=>{
         </div>
     )
 }
-
-export default Sidebar
-const Sidebar =()=>{
-    return(
-        <div className="w-[10rem] h-[40rem] bg-[#35264A]"> 
-            <img src="@/img/logodekuple.svg" alt="" />
-            <div className="">dsqds</div>
-        </div>
-    )
-}
-
-export default Sidebar
