9dba911f74e38e1ed62fa8b16256b25be73b4645
Hugo <h.pigreepro@gmail.com>
2024-05-02 16:42:40 +0200

    Description des modifications

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -3,8 +3,8 @@ const Sidebar =()=>{
         <div className="w-[10rem] h-[40rem] bg-[#35264A]"> 
             <img src="/img/logodekuple.svg" className="w-[8.75rem] h-[6.25rem] border-b-2 border-[#2BD6D9]"/>
             <div className="p-[25px]">
-                <div className="bg-black flex flex-col justify-evenly">
-                    <img src="/img/menu.svg" alt="" />
+                <div className="bg-black flex flex-col ">
+                    <img src="/img/menu.svg" alt="" className="max-w-[]"/>
                     <img src="/img/cadena.svg" alt="" />
                     <img src="/img/Vector.svg" alt="" />
                     <img src="/img/icon.svg" alt="" />
