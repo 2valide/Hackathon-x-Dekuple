654f45b672f4b6e9c684f7949462bbaf7d895340
Hugo <h.pigreepro@gmail.com>
2024-05-03 14:17:30 +0200

    Description concise de vos modifications

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,9 @@
 import Sidebar from'../app/components/Sidebar';
-
+import SidebarClose from '../app/components/SidebarClose';
 function App(){
   return(
       <div className="">
-          <Sidebar />
+          <SidebarClose />
       </div>
   );
 }
