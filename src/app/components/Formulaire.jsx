38af110697281c9bcce4c4ade1925fc4018cd2a8
Flo738 <pacardflo02@gmail.com>
2024-05-03 17:12:18 +0200

    formulaire

---
+++ src/app/components/Formulaire.jsx
@@ -0,0 +1,40 @@
+
+import Link from 'next/link';
+
+const Formulaire=() =>{
+    return(
+
+    <div className="flex flex-col justify-center items-center bg-[#2E1452] h-screen pb-[12.5rem]">
+      <h1 className=" text-2xl text-white pt-[6.25rem]  font-bold"> Général </h1>
+      <div className="w-[20.625rem] h-[23.75rem] m-auto shadow-lg bg-[#009699] rounded-md p-9">
+        <div className="mt-[0.5rem]">
+            <label for="email" className="text-white font-bold"> Entrprise </label>
+            <input type="email" className="mt-[0.1875rem] rounded-md p-[0.0625rem] w-full"/>
+            </div>
+            <div className="mt-[0.5rem]">
+            <label for="email" className="text-white font-bold"> L'url de l'Entrprise </label>
+            <input type="email" className="mt-[0.1875rem] rounded-md p-[0.0625rem] w-full"/>
+            </div>
+            <div className="mt-[1rem]">
+            <label for="login" className="text-white font-bold"> Statut </label>
+            <input type="login" className="mt-[0.1875rem] rounded-md p-[0.0625rem] w-full"/>
+            <div className="mt-[0.5rem]">
+            <label for="image" className="text-white font-bold"> Image </label>
+            <input type="image" className="mt-[0.1875rem] rounded-md p-[0.0625rem] w-full"/>
+            </div>
+        </div>
+       
+        <div className="flex justify-center items-center mt-[40px]">
+            <button className="bg-[#2E1452] text-white cursor-pointer rounded-lg p-3">
+                        Creez votre prrojet
+            </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+
+export default Formulaire 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import Tableau from '../app/components/tableaux';
 import Header from '../app/components/Header';
+import Formulaire from '../app/components/Formulaire';
+
 function App(){
   return(
       <div className="h-screen bg-black">
         <Header />
-        <Tableau />
+        <Formulaire />
       </div>
   );
 }
