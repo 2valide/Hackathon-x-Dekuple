252154243acacf2189ea4315403563278e4be642
Anthony Z <118206086+2valide@users.noreply.github.com>
2024-05-03 19:06:00 +0200

    conflit packagelock

--- src/pages/_app.jsx
+++
@@ -1,7 +0,0 @@
-import '../styles/globals.css'; // Assurez-vous que le chemin vers votre fichier CSS est correct
-
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
-
-export default MyApp;
